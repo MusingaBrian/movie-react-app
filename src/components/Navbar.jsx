a7bad8b5b9cb8e7d00551718d867ce3d7e6fb422
musinga_brian <musingabrians@gmail.com>
2024-05-19 07:21:32 +0300

    Install Chadn ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Button } from './ui/button'
 
 function navbar() {
   return (
     <div className="w-full p-3 h-[60px] border-solid border-2 border-lime-950">
         <h2 className="text-center text-lime-950 text-2xl">Movie Plex App</h2>
+        <Button>Click Me</Button>
     </div>
   )
 }
