4d09680e0da86c41714f9483730302b60018fda1
musinga_brian <musingabrians@gmail.com>
2024-05-19 06:53:41 +0300

    Implement the App.jsx, Navbar.Jsx and the omdbapi

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import {useEffect} from 'react'
+import Navbar from './components/Navbar'
+
+// c372fe48
+
+const API_KEY = "http://www.omdbapi.com?apikey=c372fe48";
 
 function App() {
+
+  const searchMovies = async (title) => {
+
+    const response = await fetch(`${API_KEY}&s=${title}`);
+    const data = await response.json();
+
+    // console.log(data.Search);
+  }
+
+  useEffect(() => {
+    searchMovies('spiderman');
+  }, [])
+  
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="w-screen bg-lime-100 h-screen">
+      <Navbar />
+    </div>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function navbar() {
+  return (
+    <div className="w-full p-3 h-[60px] border-solid border-2 border-lime-950">
+        <h2 className="text-center text-lime-950 text-2xl">Movie Plex App</h2>
+    </div>
+  )
+}
+
+export default navbar
